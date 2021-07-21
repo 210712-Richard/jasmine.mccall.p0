f742d7f70a6470f863d3715947ecbb1644c575cf
jmccall10 <68474971+jmccall10@users.noreply.github.com>
2021-07-21 10:10:50 -0500

    update

--- ProjectZero/src/main/java/com/revature/driver/Driver.java
+++ ProjectZero/src/main/java/com/revature/driver/Driver.java
@@ -6,9 +6,7 @@ public class Driver {
 		public static void main(String[] args) {
 			StartMenu m = new StartMenu();
 			m.start();
-			//writeObjectsToFile(User, "user.dat");
-			//m.shop();
-		
+
 		}
 
 }
